a560d98f5678a6643cdb2c5f8222b9cc9b8e0a5b
DOG <dog@dog.com>
2017-10-21 17:51:59 +0100

    Updated About page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -10,7 +10,10 @@ var React = require("react");
 
 var About = (props) => {
     return(
-        <h3>About Component</h3>
+        <div>
+        <h3>About</h3>
+        <h3>Welcome to About page!</h3>
+        </div>
     )
 };
 
