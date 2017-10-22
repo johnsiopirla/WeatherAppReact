4fe909267529ed5e2d847d2f3538b432bfd9e893
DOG <dog@dog.com>
2017-10-22 20:56:21 +0100

    Add Error Model

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -1,23 +1,20 @@
-var axios = require("axios");
+var axios = require('axios');
 
-const OPEN_WEATHER_MAP_URL = "http://api.openweathermap.org/data/2.5/weather?";
-const OPEN_WEATHER_MAP_URL2 = "&appid=da812360f6d41e879d20a6f12a4c51b4&units=metric";
-
+const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=c4e735ea8bd7e7b6dc8368c752517b2d&units=imperial';
 
 module.exports = {
-    getTemp: function(location) {
-        var encodedLocation = encodeURIComponent(location);
-        var requestUrl= `${OPEN_WEATHER_MAP_URL}q=${encodedLocation}${OPEN_WEATHER_MAP_URL2}`;
+  getTemp: function (location) {
+    var encodedLocation = encodeURIComponent(location);
+    var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
 
-        return axios.get(requestUrl).then(function(res){
-            debugger;
-            if(res.data.cod && res.data.message){
-                throw new Error(res.data.message)
-            }else{
-                return res.data.main.temp;
-            }
-        }, function(res){
-            throw new Error(res.data.message);
-        });
-    }
+    return axios.get(requestUrl).then(function (res) {
+      if (res.data.cod && res.data.message) {
+        throw new Error(res.data.message);
+      } else {
+        return res.data.main.temp;
+      }
+    }, function (res) {
+      throw new Error(res.data.message);
+    });
+  }
 }
