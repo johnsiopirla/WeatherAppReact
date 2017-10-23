053cd4744aeee6c96b6d6d2488f24a4d412f133d
DOG <dog@dog.com>
2017-10-23 21:56:47 +0100

    Wiring up location and example location links

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -5,7 +5,13 @@ var {Link, IndexLink} = require("react-router");
 var Nav = React.createClass({
   onSearch: function(e){
     e.preventDefault();
-    alert("not yet wired up")
+    var location = this.refs.search.value;
+    var enodedLocation = encodeURIComponent(location);
+
+    if(location.length >0){
+      this.refs.search.value = "";
+      window.location.hash="#/?location=" + enodedLocation;
+    }
   },
   render: function () {return (
     <div className = "top-bar">
@@ -27,7 +33,7 @@ var Nav = React.createClass({
         <form onSubmit={this.onSearch}>
           <ul className="menu">
             <li>
-              <input type="search" placeholder="Search weather by city"/>
+              <input ref = "search" type="search" placeholder="Search weather by city"/>
             </li>
             <li>
               <input type="submit" className="button" value="Get Weather"/>
